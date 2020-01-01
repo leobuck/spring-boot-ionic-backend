d8c8c576c91ac04ca240d4acbd95cf0b83c76264
leobuck <leobuck12@gmail.com>
2020-01-01 04:56:31 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/leo/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.leo.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leo.cursomc.domain.Cliente;
+import com.leo.cursomc.repositories.ClienteRepository;
+import com.leo.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+}
