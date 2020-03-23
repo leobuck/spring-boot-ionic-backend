e378a899800a9543e8d971b9d9569976526105bc
leobuck <leobuck12@gmail.com>
2020-03-22 22:54:33 -0300

    Enviando imagem via endpoint

--- src/main/java/com/leo/cursomc/services/ClienteService.java
+++ src/main/java/com/leo/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.leo.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.leo.cursomc.domain.Cidade;
 import com.leo.cursomc.domain.Cliente;
@@ -37,6 +39,9 @@ public class ClienteService {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public Cliente find(Integer id) {	
 		UserSS user = UserService.authenticated();
@@ -109,4 +114,7 @@ public class ClienteService {
 		newObj.setEmail(obj.getEmail());
 	}
 	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
+	}
 }
